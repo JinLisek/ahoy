6e52be1ca9a2ea454931ea419210580aab06acc5
JinLisek <tastyramen@gmail.com>
2021-08-24 22:08:09 +0200

    Add simple chat functionality from https://channels.readthedocs.io/en/stable/tutorial/part_2.html

--- ahoy_backend/ahoy_backend/asgi.py
+++ ahoy_backend/ahoy_backend/asgi.py
@@ -9,9 +9,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ahoy_backend.settings")
 
-application = ProtocolTypeRouter({"http": get_asgi_application()})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)
