0937fc9d9db961827d617cb58ff931b468207ad9
JinLisek <tastyramen@gmail.com>
2021-08-24 19:53:31 +0200

    Add simple websocket open handler in frontend

--- ahoy_frontend/src/App.js
+++ ahoy_frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.webSocketClient = new WebSocket("wss://localhost:8000/");
+  }
+
+  render = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          BLAX
+        </header>
+      </div>
+    );
+  };
+
+  openEventListener = (event) => {
+    console.log("Received open event on websocket");
+  };
+
+  componentDidMount = () => {
+    this.webSocketClient.addEventListener("open", this.openEventListener);
+  };
 }
 
 export default App;
