ee08c1482ef820d621924a1a7523c1f8c7ea58c7
JinLisek <tastyramen@gmail.com>
2021-08-27 21:00:21 +0200

    Change App to functional component which just renders based on path

--- ahoy_frontend/src/App.js
+++ ahoy_frontend/src/App.js
@@ -5,28 +5,13 @@ import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import Chat from "./components/Chat";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.webSocketClient = new WebSocket("wss://localhost:8000/");
-  }
-
-  render = () => {
-    return (
-      <div className="App">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/chat/:room_name" component={Chat} />
-      </div>
-    );
-  };
-
-  openEventListener = (event) => {
-    console.log("Received open event on websocket");
-  };
-
-  componentDidMount = () => {
-    this.webSocketClient.addEventListener("open", this.openEventListener);
-  };
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Route exact path="/" component={Home} />
+      <Route exact path="/chat/:roomName" component={Chat} />
+    </div>
+  );
+};
 
 export default App;
