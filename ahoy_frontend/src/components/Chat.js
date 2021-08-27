3eb3ac91f2a523c7ea0648057b6ea5de932f6541
JinLisek <tastyramen@gmail.com>
2021-08-27 21:00:48 +0200

    Add connecting to websocket in Chat component

--- ahoy_frontend/src/components/Chat.js
+++ ahoy_frontend/src/components/Chat.js
@@ -1,23 +1,41 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import React from "react";
 
-const Chat = (props) => {
-  return (
-    <div>
-      <h1>Room: {props.match.params.room_name}</h1>
-      <Form>
-        <Form.Group controlId="formRoomChatHistory">
-          <Form.Control as="textarea" style={{ height: "600px", width: "800px" }} />
-        </Form.Group>
-        <Form.Group controlId="formUserMessage">
-          <Form.Control as="textarea" style={{ height: "100px", width: "700px" }} />
-          <Button variant="primary" type="submit">
-            Send
-          </Button>
-        </Form.Group>
-      </Form>
-    </div>
-  );
-};
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.roomName = this.props.match.params.roomName;
+    const webSocketAddr = "ws://192.168.1.165:8000/ws/chat/" + this.roomName + "/";
+    this.webSocketClient = new WebSocket(webSocketAddr);
+  }
+
+  render = () => {
+    return (
+      <div>
+        <h1>Room: {this.roomName}</h1>
+        <Form>
+          <Form.Group controlId="formRoomChatHistory">
+            <Form.Control as="textarea" style={{ height: "600px", width: "800px" }} />
+          </Form.Group>
+          <Form.Group controlId="formUserMessage">
+            <Form.Control as="textarea" style={{ height: "100px", width: "700px" }} />
+            <Button variant="primary" type="submit">
+              Send
+            </Button>
+          </Form.Group>
+        </Form>
+      </div>
+    );
+  };
+
+  openEventListener = (event) => {
+    console.log("Received open event on websocket");
+  };
+
+  componentDidMount = () => {
+    this.webSocketClient.addEventListener("open", this.openEventListener);
+  };
+}
 
 export default Chat;
