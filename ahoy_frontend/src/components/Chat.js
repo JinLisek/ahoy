5e117e7e48f17038a0475677300bf16a72484d42
JinLisek <tastyramen@gmail.com>
2021-08-27 20:38:26 +0200

    Add simple front for room

--- ahoy_frontend/src/components/Chat.js
+++ ahoy_frontend/src/components/Chat.js
@@ -1,5 +1,23 @@
-const Chat = () => {
-  return <h1>Chat</h1>;
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const Chat = (props) => {
+  return (
+    <div>
+      <h1>Room: {props.match.params.room_name}</h1>
+      <Form>
+        <Form.Group controlId="formRoomChatHistory">
+          <Form.Control as="textarea" style={{ height: "600px", width: "800px" }} />
+        </Form.Group>
+        <Form.Group controlId="formUserMessage">
+          <Form.Control as="textarea" style={{ height: "100px", width: "700px" }} />
+          <Button variant="primary" type="submit">
+            Send
+          </Button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
 };
 
 export default Chat;
