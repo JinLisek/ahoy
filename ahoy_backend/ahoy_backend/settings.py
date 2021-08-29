0c8544942f790ced9c85ff0e25efc21d9e2220f5
JinLisek <tastyramen@gmail.com>
2021-08-29 19:44:39 +0200

    Add CORS handling to backend

--- ahoy_backend/ahoy_backend/settings.py
+++ ahoy_backend/ahoy_backend/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = "django-insecure-z!1(cj7yp(rnic1l*yc+7sz9p8u0^#m_pfea5i=49*&b!6u68#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.165"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -135,3 +137,10 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://192.168.1.165:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
