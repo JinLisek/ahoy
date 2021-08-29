936bdeb2db5f61d0cde7386ef1a6d127b5f03829
JinLisek <tastyramen@gmail.com>
2021-08-29 17:48:03 +0200

    Creation of room moved to separate component from Home

---
+++ ahoy_frontend/src/components/CreateRoom.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+class CreateRoom extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { roomName: "" };
+  }
+
+  handleChangeRoomName = (event) => {
+    this.setState({ roomName: event.target.value });
+  };
+
+  submit = (event) => {
+    event.preventDefault();
+    if (this.state.roomName !== "") this.props.history.push("/chat/" + this.state.roomName);
+  };
+
+  render = () => {
+    return (
+      <div>
+        <h1>Create new room</h1>
+        <Form onSubmit={this.submit}>
+          <Form.Group controlId={"formRoomName"}>
+            <Form.Label>Room name</Form.Label>
+            <Form.Control type="text" onChange={this.handleChangeRoomName} />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Create room
+          </Button>
+        </Form>
+      </div>
+    );
+  };
+}
+export default CreateRoom;
--- ahoy_frontend/src/components/Home.js
+++ ahoy_frontend/src/components/Home.js
@@ -3,36 +3,13 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { roomName: "" };
-  }
+import CreateRoom from "./CreateRoom";
 
-  handleChangeRoomName = (event) => {
-    this.setState({ roomName: event.target.value });
-  };
-
-  submit = (event) => {
-    event.preventDefault();
-    if (this.state.roomName !== "") this.props.history.push("/chat/" + this.state.roomName);
-  };
-
-  render = () => {
-    return (
-      <div>
-        <h1>Create new room</h1>
-        <Form onSubmit={this.submit}>
-          <Form.Group controlId={"formRoomName"}>
-            <Form.Label>Room name</Form.Label>
-            <Form.Control type="text" onChange={this.handleChangeRoomName} />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Create room
-          </Button>
-        </Form>
-      </div>
-    );
-  };
-}
+const Home = () => {
+  return (
+    <div>
+      <CreateRoom />
+    </div>
+  );
+};
 export default Home;
