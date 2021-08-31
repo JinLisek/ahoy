5568126474cbd57c6dc7ef90809be6b7a4fc53d9
JinLisek <tastyramen@gmail.com>
2021-08-31 18:15:37 +0200

    Add creating users in register view

--- ahoy_backend/authentication/views.py
+++ ahoy_backend/authentication/views.py
@@ -1,7 +1,22 @@
+from json import loads
+
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.db.models import Q
+from django.http import HttpResponse, HttpResponseBadRequest
 
 
 def register(request):
-    print(f"REGISTER: {request.body}")
-    return HttpResponse("YOU ARE REGISTERED NOT HUEHUEHUEH")
+    user_to_register = loads(request.body)
+    users_with_requested_name_or_email = User.objects.filter(
+        Q(username=user_to_register["user_name"]) | Q(email=user_to_register["email"])
+    )
+
+    if len(users_with_requested_name_or_email) == 0:
+        User.objects.create_user(
+            username=user_to_register["user_name"],
+            email=user_to_register["email"],
+            password=user_to_register["password"],
+        )
+        return HttpResponse("Correctly registered", status=201)
+
+    return HttpResponseBadRequest("User duplicated")
