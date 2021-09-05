ef27d87e7038423097103eef64a276961813a921
JinLisek <tastyramen@gmail.com>
2021-09-05 10:21:17 +0200

    Backend: search view reacts to GET request

--- ahoy_backend/search/urls.py
+++ ahoy_backend/search/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from search.views import search_view
 
-urlpatterns = [path("", search_view, name="search")]
+urlpatterns = [path("<str:search_text>/", search_view, name="search")]
--- ahoy_backend/search/views.py
+++ ahoy_backend/search/views.py
@@ -4,12 +4,11 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseBadRequest
 
 
-def search_view(request):
+def search_view(request, search_text):
     if not request.user.is_authenticated:
         return HttpResponseBadRequest("Not logged in, cannot search")
 
-    search_request = json.loads(request.body)
-    found_users = User.objects.filter(username__contains=search_request["search_text"])
+    found_users = User.objects.filter(username__contains=search_text)
 
     if len(found_users) == 0:
         return HttpResponseBadRequest("Nothing found")
