094192e7cf1b480b1d5cab7f1761d80d19f15076
JinLisek <tastyramen@gmail.com>
2021-09-19 13:43:05 +0200

    Frontend: add default avatar

--- ahoy_frontend/src/components/UserInformation.js
+++ ahoy_frontend/src/components/UserInformation.js
@@ -1,11 +1,21 @@
 import { connect } from "react-redux";
 
+import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const UserInformation = (props) => {
   return (
     <Container>
-      <p>Logged as: {props.userInfo.username}</p>
+      <Row>
+        <Col>
+          <Image width={50} height={50} src={process.env.PUBLIC_URL + "/ahoy-default-avatar.png"} />
+        </Col>
+        <Col>
+          <p className="text-right">{props.userInfo.username}</p>
+        </Col>
+      </Row>
     </Container>
   );
 };
