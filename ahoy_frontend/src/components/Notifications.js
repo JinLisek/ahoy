d86480be201aab600c1a932902bf7406f821eb16
JinLisek <tastyramen@gmail.com>
2021-09-19 14:22:26 +0200

    Frontend: Notifications sends asks for friend  requests and lists them in dropdown

--- ahoy_frontend/src/components/Notifications.js
+++ ahoy_frontend/src/components/Notifications.js
@@ -1,3 +1,39 @@
-const Notifications = () => <div></div>;
+import React from "react";
+
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+import { getBackend } from "../common/BackendApiUtilities";
+
+class Notifications extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { friendRequests: [] };
+  }
+
+  componentDidMount = async () => {
+    await this.requestNotifications();
+  };
+
+  requestNotifications = async () => {
+    try {
+      const notificationsResp = await getBackend("friends");
+      const { data } = await notificationsResp;
+      this.setState({ friendRequests: data.friend_requests });
+    } catch (err) {
+      if (err.response) {
+        const errResp = err.response;
+        console.error(`${errResp.statusText}: ${errResp.data}`);
+      } else console.error(err);
+    }
+  };
+
+  render = () => (
+    <NavDropdown title="Notifications">
+      {this.state.friendRequests.map((requester) => (
+        <NavDropdown.Item key={"friendRequestFrom" + requester}>{requester}</NavDropdown.Item>
+      ))}
+    </NavDropdown>
+  );
+}
 
 export default Notifications;
