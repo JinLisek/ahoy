a4c078ad5c6c41b086732a0b2f2cb7d238c27f92
JinLisek <tastyramen@gmail.com>
2021-10-27 18:04:05 +0200

    Frontend: move creation of web socket to BackendApiUtilities

--- ahoy_frontend/src/common/BackendApiUtilities.js
+++ ahoy_frontend/src/common/BackendApiUtilities.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 import Cookies from "universal-cookie";
 
-const BACKEND_PROTOCOL = "http";
 const BACKEND_ADDRESS = "192.168.1.165";
 const BACKEND_PORT = "8000";
 
-const createBackendUrl = (target) => BACKEND_PROTOCOL + "://" + BACKEND_ADDRESS + ":" + BACKEND_PORT + "/" + target;
+const createBackendUrl = (protocol, target) => protocol + "://" + BACKEND_ADDRESS + ":" + BACKEND_PORT + "/" + target;
 
 const createBackendApi = () => {
   const cookies = new Cookies();
@@ -17,12 +16,28 @@ const createBackendApi = () => {
   });
 };
 
+const onSocketOpen = (event) => {
+  console.info("WebSocket was opened by server");
+};
+
+const onSocketClose = (event) => {
+  console.warn("WebSocket was closed by server");
+};
+
 export const postBackend = async (target, data) => {
   const backendApi = createBackendApi();
-  return await backendApi.post(createBackendUrl(target), data);
+  return await backendApi.post(createBackendUrl("http", target), data);
 };
 
 export const getBackend = async (target) => {
   const backendApi = createBackendApi();
-  return await backendApi.get(createBackendUrl(target));
+  return await backendApi.get(createBackendUrl("http", target));
+};
+
+export const createWebSocket = (target, onMsg) => {
+  const socket = new WebSocket(createBackendUrl("ws", target));
+  socket.addEventListener("open", onSocketOpen);
+  socket.onmessage = onMsg;
+  socket.onclose = onSocketClose;
+  return socket;
 };
--- ahoy_frontend/src/components/Chat/Chat.js
+++ ahoy_frontend/src/components/Chat/Chat.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { createWebSocket } from "../../common/BackendApiUtilities";
+
 import ChatView from "./ChatView";
 
 class Chat extends React.Component {
@@ -22,10 +24,6 @@ class Chat extends React.Component {
     this.setState({ userMessage: event.target.value });
   };
 
-  openEventListener = (event) => {
-    console.log("Received open event on websocket");
-  };
-
   onMessageReceived = (event) => {
     const data = JSON.parse(event.data);
     this.setState((prevState) => {
@@ -34,14 +32,7 @@ class Chat extends React.Component {
   };
 
   componentDidMount = () => {
-    const webSocketAddr = "ws://192.168.1.165:8000/ws/chat/" + this.roomName + "/";
-    this.webSocketClient = new WebSocket(webSocketAddr);
-    this.webSocketClient.onmessage = this.onMessageReceived;
-
-    this.webSocketClient.onclose = function (e) {
-      console.error("Chat socket closed unexpectedly");
-    };
-    this.webSocketClient.addEventListener("open", this.openEventListener);
+    this.webSocketClient = createWebSocket("ws/chat/" + this.roomName + "/", this.onMessageReceived);
   };
 
   render = () => (
