85927eb1708f7fa8c09cbeae5dfcdc8d3546bd46
JinLisek <tastyramen@gmail.com>
2021-10-27 18:58:35 +0200

    Backend: beginning of fixing pylint

--- ahoy_backend/ahoy_backend/asgi.py
+++ ahoy_backend/ahoy_backend/asgi.py
@@ -11,8 +11,9 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from chat.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
+
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ahoy_backend.settings")
 
