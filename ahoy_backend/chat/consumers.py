32e619726915ddaabefe451bb2ce7845463d2d14
JinLisek <tastyramen@gmail.com>
2021-10-27 16:43:55 +0200

    Backend: add author to chat messages

--- ahoy_backend/chat/consumers.py
+++ ahoy_backend/chat/consumers.py
@@ -4,26 +4,53 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = "chat_%s" % self.room_name
+    def __init__(self):
+        super().__init__()
+        self.__room_name = None
+        self.__user = None
 
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+    async def connect(self):
+        self.__user = self.scope["user"]
+        self.__room_name = self.scope["url_route"]["kwargs"]["room_name"]
+
+        if not self.__user.is_authenticated:
+            print("Unathenticated user tried to connect to chat")
+            return
+
+        await self.channel_layer.group_add(self.__room_name, self.channel_name)
 
         await self.accept()
 
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+    async def disconnect(self, code):
+        del code  # unused
 
-    async def receive(self, text_data):
+        if self.__room_name is None:
+            return
+
+        await self.channel_layer.group_discard(self.__room_name, self.channel_name)
+
+    async def receive(self, text_data=None, bytes_data=None):
+        if not self.__user.is_authenticated:
+            print("Received chat msg from unathenticated user")
+            return
+
+        del bytes_data  # unused
+
+        if self.__room_name is None:
+            return
+
         text_data_json = json.loads(text_data)
+        author = self.__user.username
         message = text_data_json["message"]
 
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": message}
+            self.__room_name,
+            {"type": "chat_message", "author": author, "message": message},
         )
 
     async def chat_message(self, event):
-        message = event["message"]
-
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(
+            text_data=json.dumps(
+                {"author": event["author"], "message": event["message"]}
+            )
+        )
