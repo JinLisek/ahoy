9178d76411f7e02ccca77a26341f5487f94d530e
JinLisek <tastyramen@gmail.com>
2021-12-10 20:28:10 +0100

    Backend: add test covering correct login

--- ahoy_backend/authentication/tests/test_login_view.py
+++ ahoy_backend/authentication/tests/test_login_view.py
@@ -1,5 +1,5 @@
 import pytest
-from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
 LOGIN_PATH = "/authentication/login"
 
@@ -23,6 +23,28 @@ def test_when_trying_to_login_with_incorrect_credentials_should_return_bad_reque
     )
 
     assert HTTP_400_BAD_REQUEST == response.status_code
+
+
+@pytest.mark.django_db
+def test_when_trying_to_login_with_correct_credentials_should_return_status_ok(
+    client, django_user_model
+):
+    username = "someusername"
+    password = "userpassword"
+
+    django_user_model.objects.create_user(
+        username=username,
+        email="some@email.domain",
+        password=password,
+    )
+
+    response = client.post(
+        LOGIN_PATH,
+        data={"username": username, "password": password},
+        content_type="application/json",
+    )
+
+    assert HTTP_200_OK == response.status_code
 
 
 @pytest.mark.django_db
