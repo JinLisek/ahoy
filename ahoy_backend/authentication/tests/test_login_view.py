6bc33efb1eb67fc503326d487f272776646bfd00
JinLisek <tastyramen@gmail.com>
2021-12-10 20:07:51 +0100

    Backend: replace test creator of login requests with client (from pytest-django)

--- ahoy_backend/authentication/tests/test_login_view.py
+++ ahoy_backend/authentication/tests/test_login_view.py
@@ -1,33 +1,26 @@
 import pytest
 from rest_framework.status import HTTP_400_BAD_REQUEST
 
-from authentication.views import login_view
-
-LOGIN_PATH = "authentication/login"
-
-
-@pytest.fixture(name="create_post_login_request")
-def fixture_create_post_login_request(rf):
-    def create_request(body=None):
-        request = rf.post(path=LOGIN_PATH, data=body, content_type="application/json")
-        return request
-
-    return create_request
+LOGIN_PATH = "/authentication/login"
 
 
 @pytest.mark.django_db
-def test_given_post_request_with_incorrect_credentials_should_return_bad_request(
-    create_post_login_request, django_user_model
+def test_when_trying_to_login_with_incorrect_credentials_should_return_bad_request(
+    client, django_user_model
 ):
     username = "some username"
-    incorrect_credentials = {"username": username, "password": "incorrect password"}
+
     django_user_model.objects.create_user(
         username=username,
         email="some@email.domain",
         password="correct password",
     )
 
-    response = login_view(request=create_post_login_request(body=incorrect_credentials))
+    response = client.post(
+        LOGIN_PATH,
+        data={"username": username, "password": "incorrect password"},
+        content_type="application/json",
+    )
 
     assert HTTP_400_BAD_REQUEST == response.status_code
 
@@ -38,9 +31,9 @@ def test_given_post_request_with_incorrect_credentials_should_return_bad_request
     ["put", "head", "delete", "options", "trace", "patch"],
 )
 def test_given_unsupported_http_method_should_return_bad_request(
-    unsupported_method, rf
+    client, unsupported_method
 ):
-    create_request = getattr(rf, unsupported_method)
-    response = login_view(request=create_request(path=LOGIN_PATH))
+    unsupported_request = getattr(client, unsupported_method)
+    response = unsupported_request(LOGIN_PATH)
 
     assert HTTP_400_BAD_REQUEST == response.status_code
